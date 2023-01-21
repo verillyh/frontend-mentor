3fa299e1a251f3667008efd7f700f5337f8966ac
verilly <verillytmt@gmail.com>
2023-01-21 15:29:55 +0800

    - Added javascript: Added event listener to faq for click toggle active class when clicked - Added CSS: Added active classes

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function textToBold (item) {
+// When the page is loaded...
+document.addEventListener("DOMContentLoaded", () => {
+    faqList = document.getElementsByClassName("faq-title");
 
-}
+    for (let i = 0; i < faqList.length; i++) {
+        faqList[i].addEventListener("click", function () {
+            // Apply active state when clicked
+            this.classList.toggle("title-active");
+            this.nextElementSibling.classList.toggle("desc-active");
+            this.parentElement.classList.toggle("parent-active");
+        });
+    }
+})
