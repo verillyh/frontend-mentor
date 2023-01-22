bcfe95fec523fb21afc4f73347b966b78888f478
verilly <verillytmt@gmail.com>
2023-01-22 17:03:00 +0800

    Added JS code to only limit to 1 description pop up at a time

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
+function toggleActive (element) {
+    element.parentElement.classList.toggle("parent-active");
+    element.nextElementSibling.classList.toggle("desc-active");
+    element.classList.toggle("title-active");
+}
+
+
 // When the page is loaded...
 document.addEventListener("DOMContentLoaded", () => {
     faqList = document.getElementsByClassName("faq-title");
+    length = faqList.length;
 
-    for (let i = 0; i < faqList.length; i++) {
+    for (let i = 0; i < length; i++) {
         faqList[i].addEventListener("click", function () {
-            // Apply active state when clicked
-            this.classList.toggle("title-active");
-            this.nextElementSibling.classList.toggle("desc-active");
-            this.parentElement.classList.toggle("parent-active");
+            // Get which element was active before
+            activeElement = document.querySelector(".title-active");
+
+            // If no element was clicked before..
+            if (activeElement == null) {
+                toggleActive(this);
+            }
+            else {
+                // Inactive the element before
+                toggleActive(activeElement);
+
+                // To simultaneously toggle new active element when new element is clicked
+                if (activeElement != this) {
+                    toggleActive(this);
+                }
+            }
         });
     }
 })
