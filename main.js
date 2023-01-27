a8e7b13e18676afac0a0e580285350053a1ea02c
verilly <verillytmt@gmail.com>
2023-01-27 20:37:54 +0800

    Added faq-items functionality: --> Hover on faq-titles changes text color --> Added animations on pop up description and rotate arrow Added one media query on main tag CSS #TODO: Add media queries for the mobile look

--- main.js
+++ main.js
@@ -1,33 +1,61 @@
-function toggleActive (element) {
-    element.parentElement.classList.toggle("parent-active");
-    element.nextElementSibling.classList.toggle("desc-active");
-    element.classList.toggle("title-active");
+/* || Arrow functions */
+function addArrow (element) {
+    /* Give arrow svg */
+    var arrow = document.createElement("img");
+    arrow.classList.add("arrow");
+    arrow.setAttribute("src", "./images/icon-arrow-down.svg");
+
+    element.append(arrow);
+}
+
+function transfromArrow (arrow) {
+    arrow.classList.toggle("rotate");
+}
+
+/* || Active functions  */
+function toggleFaqActive (item) {
+    /* Set faq items to active mode */
+    item.parentElement.classList.toggle("parent-active");
+    item.nextElementSibling.classList.toggle("desc-active");
+    item.classList.toggle("title-active");
+}
+
+function listenForClicks (items) {
+    /* Listen for click events -> faq-items */
+    length = items.length;
+
+    for (let i = 0; i < length; i++) {
+        addArrow(items[i]);
+
+        items[i].addEventListener("click", function () {
+            // Get which element was active before
+            activeElement = document.querySelector(".title-active");
+
+            // If no element was clicked before..
+            if (activeElement == null) {
+                toggleFaqActive(this);
+                transfromArrow(this.lastChild);
+            }
+            else {
+                // Inactive the element before
+                toggleFaqActive(activeElement);
+                transfromArrow(activeElement.lastChild);
+
+                // To simultaneously toggle new active element when new element is clicked
+                if (activeElement != this) {
+                    toggleFaqActive(this);
+                    transfromArrow(this.lastChild);
+                }
+            }
+        });
+    }
 }
 
 
 // When the page is loaded...
 document.addEventListener("DOMContentLoaded", () => {
     faqList = document.getElementsByClassName("faq-title");
-    length = faqList.length;
+    
+    listenForClicks(faqList);
 
-    for (let i = 0; i < length; i++) {
-        faqList[i].addEventListener("click", function () {
-            // Get which element was active before
-            activeElement = document.querySelector(".title-active");
-
-            // If no element was clicked before..
-            if (activeElement == null) {
-                toggleActive(this);
-            }
-            else {
-                // Inactive the element before
-                toggleActive(activeElement);
-
-                // To simultaneously toggle new active element when new element is clicked
-                if (activeElement != this) {
-                    toggleActive(this);
-                }
-            }
-        });
-    }
 })
