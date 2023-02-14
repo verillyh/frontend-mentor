6258159eb6e76cf17d8147985f59fc451cb9a645
verillyh <verillytmt@gmail.com>
2023-02-14 21:02:17 +0800

    Upper part done, now onto testimonials section

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-    var reviews = document.getElementsByClassName("rating-item");
-    var starNumber = 5;
-    var stars = new Array(starNumber);
-
-    for (let i = 0; i < reviews.length; i++) {
-        let review = reviews[i];
-        let parent = review.parentNode;
-        for (let j = 0; j < starNumber; j++) {
-            stars[j] = document.createElement("div");
-            stars[j].classList.add("star");
-
-            parent.insertBefore(stars[j], review);
+function addStars(stars) {
+    var length = stars.length;
+    let starNumber = 5;
+    
+    for (let i = 0; i < length; i++)
+    {        
+        for (let j = 0; j < starNumber; j++)
+        {
+            let star = document.createElement("div");
+            star.classList.add("star");
+            stars[i].appendChild(star);
         }
     }
+}
+
+function offsideReview(reviews) {
+    let offsideValue = 50;
+    let length = reviews.length;
+
+    for (let i = 0; i < length; i++)
+    {
+        reviews[i].style.marginLeft = offsideValue * i + "px";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    var stars = document.getElementsByClassName("stars");
+    var reviews = document.getElementsByClassName("rating-item");
+
+    addStars(stars);
+    offsideReview(reviews);
 })
